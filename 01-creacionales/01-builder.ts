88fe41d1fe7e8ada85f4a42dc5caa36f328cd29b
JaViKo500 <javikogutierrez64@gmail.com>
2025-02-23 21:12:01 -0500

    create builder class computer

--- 01-creacionales/01-builder.ts
+++ 01-creacionales/01-builder.ts
@@ -12,3 +12,53 @@
  *
  * https://refactoring.guru/es/design-patterns/builder
  */
+
+class Computer {
+  public cpu: string = 'cpu - not defined';
+  public ram: string = 'ram - not defined';
+  public storage: string = 'storage - not defined';
+  public gpu?: string;
+
+  displayConfiguration = () => {
+    console.log('<--------------- JK 01-builder --------------->');
+    console.log(`
+      CPU: ${this.cpu}
+      RAM: ${this.ram}
+      Storage: ${this.storage}
+      GPU: ${this.gpu}`
+    );
+  }
+}
+
+
+class ComputerBuilder {
+  private computer: Computer;
+
+  constructor() {
+    this.computer = new Computer();
+  }
+
+  setCpu(cpu: string): ComputerBuilder {
+    this.computer.cpu = cpu;
+    return this;
+  }
+
+  setRam(ram: string): ComputerBuilder {
+    this.computer.ram = ram;
+    return this;
+  }
+
+  setStorage(storage: string): ComputerBuilder {
+    this.computer.storage = storage;
+    return this;
+  }
+
+  setGpu(gpu: string): ComputerBuilder {
+    this.computer.gpu = gpu;
+    return this;
+  }
+
+  build() {
+    return this.computer;
+  }
+}
