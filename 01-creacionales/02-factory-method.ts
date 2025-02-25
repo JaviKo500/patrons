e6fb1871d1ccd4e13b54aaa2d16286bad6c4c97e
JaViKo500 <javikogutierrez64@gmail.com>
2025-02-24 22:42:22 -0500

    check factory method

--- 01-creacionales/02-factory-method.ts
+++ 01-creacionales/02-factory-method.ts
@@ -1,3 +1,4 @@
+import { COLORS } from '../helpers/colors.ts';
 /**
  * ! Factory Method:
  * El patrón Factory Method permite crear objetos sin especificar
@@ -12,3 +13,80 @@
  * https://refactoring.guru/es/design-patterns/factory-method
  *
  */
+
+interface Hamburger {
+  prepare(): void;
+}
+
+class ChickenBurger implements Hamburger {
+  prepare(): void {
+    console.log('<--------------- JK 02-factory-method --------------->');
+    console.log('Preparing %ChickenBurger', COLORS.cyan);
+  }
+}
+
+class BeefBurger implements Hamburger {
+  prepare(): void {
+    console.log('<--------------- JK 02-factory-method --------------->');
+    console.log('Preparing %BeefBurger', COLORS.green);
+  }
+}
+
+class BeanHamburger implements Hamburger {
+  prepare(): void {
+    console.log('<--------------- JK 02-factory-method --------------->');
+    console.log('Preparing %BeanBurger', COLORS.yellow);
+  }
+}
+
+abstract class Restaurant {
+  abstract createHamburger(): Hamburger;
+
+  orderHamburger(): void {
+    const hamburger = this.createHamburger();
+    hamburger.prepare();
+  }
+}
+
+class ChickenRestaurant extends Restaurant {
+  createHamburger(): Hamburger {
+    return new ChickenBurger();
+  }
+}
+
+class BeefRestaurant extends Restaurant {
+  createHamburger(): Hamburger {
+    return new BeefBurger();
+  }
+}
+
+class BeanRestaurant extends Restaurant {
+  createHamburger(): Hamburger {
+    return new BeanHamburger();
+  }
+}
+
+function main() {
+  let restaurant: Restaurant;
+
+  const burgerType = prompt('What kind of burger do you want(chicken/beef/bean)?');
+  
+  switch (burgerType) {
+    case 'chicken':
+      restaurant = new ChickenRestaurant();
+      break;
+    case 'beef':
+      restaurant = new BeefRestaurant();
+      break;
+    case 'bean':
+      restaurant = new BeanRestaurant();
+      break;
+    default:
+     throw new Error("Invalid burger type");
+     
+  }
+
+  restaurant.orderHamburger();
+}
+
+main();
