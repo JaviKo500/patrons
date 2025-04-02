e65cc315993c5c443439e38ce7754c2498c65c0d
JaViKo500 <javikogutierrez64@gmail.com>
2025-04-01 23:19:13 -0500

    feat: bridge patron

--- 02-estructurales/02-bridge.ts
+++ 02-estructurales/02-bridge.ts
@@ -9,3 +9,78 @@
  *
  * https://refactoring.guru/es/design-patterns/bridge
  */
+
+interface Ability {
+  use(): void;
+}
+
+class SwordAttack implements Ability {
+  use() {
+    console.log('Sword attack');
+  }
+}
+
+class MagicSpell implements Ability {
+  use() {
+    console.log('Magic spell');
+  }
+}
+
+class AxeAttack implements Ability {
+  use() {
+    console.log('Axe attack');
+  }
+}
+
+class Fireball implements Ability {
+  use() {
+    console.log('Fireball');
+  }
+}
+
+abstract class Character {
+  protected ability: Ability;
+
+  constructor(ability: Ability) {
+    this.ability = ability;
+  }
+
+  setAbility(ability: Ability) {
+    this.ability = ability;
+  }
+
+  abstract performAbility(): void;
+}
+
+class Warrior extends Character {
+  override performAbility(): void {
+    console.log('<--------------- JK 02-bridge --------------->');
+    console.log('Warrior performs ability');
+    this.ability.use();
+  }
+}
+
+class Mage extends Character {
+  override performAbility(): void {
+    console.log('<--------------- JK 02-bridge --------------->');
+    console.log('Mage performs ability');
+    this.ability.use();
+  }
+}
+
+function main() {
+  const warrior = new Warrior(new SwordAttack());
+  warrior.performAbility();
+
+  warrior.setAbility(new AxeAttack());
+  warrior.performAbility();
+
+  const mage = new Mage(new MagicSpell());
+  mage.performAbility();
+
+  mage.setAbility(new Fireball());
+  mage.performAbility();
+
+}
+
+main();
